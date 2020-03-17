58d497ba7570190dbee4c6388e84dbe803e4515b
garritfra <garritfranke@gmail.com>
2020-03-17 15:46:41 +0100

    Add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,15 @@ module.exports = {
       resolve: `gatsby-theme-blog`,
       options: {},
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-122968527-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+      },
+    },
   ],
   pathPrefix: "/blog",
   // Customize your site metadata:
